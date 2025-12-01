fe7e915171bd2b7843d42193d1d33897c6bff1bf
tajdini <tajdini@gamelectronics.com>
2025-12-01 18:45:08 +0330

    initial commit

---
+++ app/src/main/java/com/the_tj/bizcard/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.the_tj.bizcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.the_tj.bizcard.ui.theme.BizCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BizCardTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateBizCard(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BizCardTheme {
+        Greeting("Android")
+    }
+}
